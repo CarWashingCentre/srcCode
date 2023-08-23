9702fb00cba81ab3f37989a43c9b25036c55cd4c
NikhilNaik <111115551+NikhilNaik21@users.noreply.github.com>
2023-08-23 13:00:09 +0530

    Create AdminHeader.js

---
+++ AdminHeader.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import 
+ {BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify}
+ from 'react-icons/bs'
+import './Common.css';
+function Header({OpenSidebar}) {
+  return (
+    <header className='header'>
+        <div className='menu-icon'>
+            <BsJustify className='icon' onClick={OpenSidebar}/>
+        </div>
+        {/* <div className='header-left'>
+            <BsSearch  className='icon'/>
+        </div> */}
+        <div className='header-right'>
+            <BsFillBellFill id="Icon1" className='icon'/>
+            <BsFillEnvelopeFill className='icon'/>
+            <BsPersonCircle className='icon'/>
+        </div>
+    </header>
+  )
+}
+
+export default Header;
